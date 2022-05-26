4af023d1fa3499f3d363af081996edaa693c9abc
Bryce Hackel <34104885+bhackel@users.noreply.github.com>
2022-05-25 21:01:19 -0700

    unfinished update

--- classdrops.py
+++ classdrops.py
@@ -3,29 +3,41 @@ import requests
 from bs4 import BeautifulSoup as BS
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+import win10toast
+import dotenv
+from dotenv import dotenv_values
 
-courses = input("Type the courses here, then push enter:    ")
+toast = win10toast.ToastNotifier()
+config = dotenv.dotenv_values("credentials.env")
+print(config)
+
+
+with open("courses.txt") as f:
+    courses = f.read().strip()
+if len(courses) == 0:
+    assert "courses.txt is empty, add some course names."
 
 headers = {
-    'Connection': 'keep-alive',
-    'Cache-Control': 'max-age=0',
-    'sec-ch-ua': '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
-    'sec-ch-ua-mobile': '?0',
-    'Upgrade-Insecure-Requests': '1',
-    'Origin': 'https://act.ucsd.edu',
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'Sec-Fetch-Site': 'same-origin',
-    'Sec-Fetch-Mode': 'navigate',
-    'Sec-Fetch-User': '?1',
-    'Sec-Fetch-Dest': 'document',
-    'Referer': 'https://act.ucsd.edu/scheduleOfClasses/scheduleOfClassesStudent.htm',
     'Accept-Language': 'en-US,en;q=0.9',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive',
+    'Origin': 'https://act.ucsd.edu',
+    'Pragma': 'no-cache',
+    'Referer': 'https://act.ucsd.edu/scheduleOfClasses/scheduleOfClassesStudentResult.htm',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'same-origin',
+    'Sec-Fetch-User': '?1',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36',
+    'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="101", "Google Chrome";v="101"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
 }
 
 data = [
-    ('selectedTerm', 'FA21'),
+    ('selectedTerm', 'FA23'),
     ('xsoc_term', ''),
     ('loggedIn', 'false'),
     ('tabNum', 'tabs-crs'),
@@ -104,11 +116,15 @@ data = [
 server = None
 
 while True:
+    timestamp = time.strftime('%a %H:%M:%S')
+    print(timestamp)
+
     # Get HTML response from link with selected classes inputted
     url = 'https://act.ucsd.edu/scheduleOfClasses/scheduleOfClassesStudentResult.htm'
     response = requests.post(url, headers=headers, data=data, timeout=10)
     soup = BS(response.text, 'html.parser')
 
+    # Find all sections for classes using specific class tag
     section_all = soup.find_all('tr', {'class': 'sectxt'})
     for section in section_all:
         col = section.find_all('td')
@@ -123,6 +139,10 @@ while True:
         if "FULL" not in resource_text and len(resource_text) > 0:
             print("Open class found")
             print("Trying to send message")
+
+            # First send windows notification
+            toast.show_toast("ClassDrops Bot",f"Open class found: {courses}",duration=5)
+
             try:
                 # Fetch details from file
                 with open('nudes.txt', 'r') as f:
@@ -146,7 +166,7 @@ while True:
                 # Get 4 letter class ID using an excessively long regex
                 class_id = re.search('subject=....', col[12].find('span')['onclick'])[0][-4:]
 
-                message = f'Open class {class_id} of ID {section_id} on {day} at {time_} in {building} with {prof}'
+                message = f'Open class {class_id}, ID {section_id}, {day}, {time_} with {prof}'
 
                 # Format everything using MIME
                 msg = MIMEMultipart()
@@ -165,5 +185,5 @@ while True:
             except Exception as e:
                 print("Something failed when sending a message:", e)
 
-    print("Checking again in 120 seconds\n")
-    time.sleep(120)
+    print("Checking again in 180 seconds\n")
+    time.sleep(180)
